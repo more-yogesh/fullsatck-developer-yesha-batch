9adacdf2b05601ab827f403a2f274cbb331c72dc
Yogesh <yogeshmore044@gmail.com>
2021-01-21 16:31:20 +0530

    Day one basics

---
+++ basic/day1.php
@@ -0,0 +1,63 @@
+<?php
+// variable declaration
+// 4 rules
+// 1. special character not allowed
+// 2. Cant start with number
+// 3. Variable name shouldnt be keyword
+// 4. names are case sensitive
+
+// a = c + b;
+
+$a = 10;
+
+echo $a;
+echo "Hello World";
+
+// operators
+
+//10/2
+
+
+$x = 10;
+$y = 2;
+
+// echo $x%$y;
+
+
+// let have a fun with increment and decrement
+
+echo "<hr/>";
+
+$i = 0;
+
+// echo $i++;
+// echo ++$i;
+// echo $i;
+
+// echo $i++ + ++$i;
+
+echo ++$i + ++$i;
+echo "<hr/>";
+
+?>
+<html>
+
+<head>
+    <title>Basic PHP Syntax</title>
+</head>
+
+<body bgcolor="red">
+    <h1>I am header</h1>
+    <h2>I am header</h2>
+    <h3>I am header</h3>
+    <h4>I am header</h4>
+    <h5>I am header</h5>
+    <h6>I am header</h6>
+    <span>Hello &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &copy;&reg; Guys</span>
+    <span>World</span>
+    <p>My Name</p>
+    <p>Yogesh</p>
+    <!-- <h7>I am header</h7> -->
+</body>
+
+</html>
