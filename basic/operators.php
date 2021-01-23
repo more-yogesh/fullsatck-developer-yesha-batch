a4fe03b8fb08355929d868861d454e5ca00d030a
Yogesh <yogeshmore044@gmail.com>
2021-01-23 17:49:13 +0530

    operator, loops and logical operator

---
+++ basic/operators.php
@@ -0,0 +1,101 @@
+<?php
+// echo 5 % 10;
+
+// echo 10 % 2;
+
+// $i = 1;
+
+// echo $i++;
+
+// echo $i;
+
+// echo 10 == 10;
+
+$check = false;
+
+// if (10 === '10') {
+//     echo "Hello World";
+// }
+
+
+// if (10 != '10') {
+//     echo "Hello World";
+// }
+
+if (1 == 1 && 10 < 50) {
+    // echo "Both Are TRUE";
+}
+
+if (1 == 1 || 5 < 10) {
+    // echo "One Must have to be true";
+}
+$a = 'male';
+$b = 'female';
+$c = 'baby';
+// if (($a == 'male' && $b == 'female') || $c = 'child') {
+//     echo "All Okay";
+// }
+
+// echo 10 * 5 + 2; BODMAS
+
+
+
+
+/**
+ * 
+  =
+  ==
+  ===
+ */
+
+// echo (1 == 1 && 10 < 50) ? 'I am true' : 'I am false';
+
+/**
+ * 
+ * 1. initialization
+ * 2. condition checking
+ * 3. increment/decrement
+ */
+
+// for ($k = 1; $k <= 10; $k++) {
+//     echo $k . "<br/>";
+// }
+
+// for ($o = 1; $o <= 3; $o++) {
+//     echo $o;
+//     for ($w = 1; $w <= 4; $w++) {
+//         echo "I am W <br/>";
+//     }
+// }
+
+// *
+// **
+// ***
+// ****
+// *****
+
+// echo str_repeat("YOGESH", 10);
+
+// for ($s = 1; $s <= 5; $s++) {
+//     for ($ss = 1; $ss <= $s; $ss++) {
+//         echo "*";
+//     }
+//     echo "<br/>";
+// }
+
+// for ($s = 1; $s <= 5; $s++) {
+//     echo str_repeat("*", $s);
+//     echo "<br/>";
+// }
+
+$vr = -10;
+
+if ($vr == -100) {
+    echo "I am true";
+} elseif ($vr < 0) {
+    echo "I am else if";
+} else {
+    echo "DEFAULT EXECute";
+}
+
+echo "Hello World";
